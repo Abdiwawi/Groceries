3fe627974358a7c1844ec72222ae766f4bd563b2
Abdiwawi <abdijamzilan@gmail.com>
2017-04-30 22:33:55 +0300

    add item numbering

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#food form").submit(function(event){
+    var Item1 = ("input#Item1").val();
+    var Item2 = ("input#Item2").val();
+    var Item3 = ("input#Item3").val();
+    var Item4 = ("input#Item4").val();
+    var Item5 = ("input#Item5").val();
+    var Item6 = ("input#Item6").val();
+    var Item7 = ("input#Item7").val();
+    var Item8 = ("input#Item8").val();
+    var Item9 = ("input#Item9").val();
+    var Item10 = ("input#Item10").val();
+
+  }
+}
